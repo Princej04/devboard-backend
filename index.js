2ee6a45bda194cba1c5dd5ff89c898df598ad67d
pj1322111@gmail.com <pj1322111@gmail.com>
2026-05-31 00:23:15 +0530

    remove trailing slash from cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://devboard-frontend-chi.vercel.app/'  // replace with your actual Vercel URL
+    'https://devboard-frontend-chi.vercel.app'  // replace with your actual Vercel URL
   ],
   credentials: true
 };
